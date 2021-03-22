53e08ef52aed363484c79771d6e9539b17124d8a
agarwalmohak6 <63978064+agarwalmohak6@users.noreply.github.com>
2021-03-22 15:09:41 +0530

    armstrongcheck.py

--- armstrongcheck.py
+++ armstrongcheck.py
@@ -1,9 +1,30 @@
+#Using while loop
+
+
 n=input()
 ln=len(n)
 s=0
-for i in n:
-    s+=int(i)**ln
+i=0
+while(i<ln):
+    print(n[i])
+    s+=int(n[i])**ln
+    i+=1
 if int(n)==s:
     print("Armstrong")
 else:
     print("Not Armstrong number")
+
+
+#Using for loop
+
+
+#n=input()
+#ln=len(n)
+#s=0
+#for i in n:
+#    s+=int(i)**ln
+#if int(n)==s:
+#    print("Armstrong")
+#else:
+#    print("Not Armstrong number")
+
